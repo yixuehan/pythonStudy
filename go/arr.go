92b84333caa0a5fd94666b315439300adae2bcf7
yixuehan <505703394@qq.com>
2018-07-10 03:58:48 -0700

    go

---
+++ go/arr.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func modify(arr [] int){
+    arr[0] = 100
+    fmt.Println("len:", len(arr))
+    fmt.Println("modify:", arr)
+}
+
+func main(){
+    var arr [10]int = [10]int{1,2,3,4,5,6,7,8,9,0}
+    var slice_arr []int = arr[:5]
+    modify(slice_arr)
+    fmt.Println("after modify:", arr)
+    fmt.Println("after modify:", slice_arr)
+}
