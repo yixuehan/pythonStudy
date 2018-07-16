42167bbf903aa44687ce88cd2109b7eef60d844f
yixuehan <505703394@qq.com>
2018-07-15 23:04:46 -0700

    go

--- go/func.go
+++ go/func.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "./pkg"
+
+)
 
 func delta(old, new int) int {
     return new - old
 }
 
 func main() {
+    pack2.Foo()
     fmt.Println(delta(10, 20))
 }
