de5e4eee77171a94a4a9d9e78e679815867b8dd3
yixuehan <505703394@qq.com>
2019-03-01 02:41:57 -0800

    save

--- python/lhc.py
+++ python/lhc.py
@@ -9,7 +9,7 @@ itchat.auto_login(hotReload = True, enableCmdQR=2)
 
 members = itchat.get_friends(update = True)
 
-f = open("frends.txt", "w")
+f = open("friends.txt", "w")
 
 for m in members:
     f.write(str(m) + "\n")
@@ -18,17 +18,36 @@ f.close()
 
 i = 0
 
-ms = members.copy()
-ms.clear()
+ms = []
+
+nickNames = ['何继中 智能焊接机器人', 'LHC', '三栖', '燕儿翩翩飞']
 
 #ms.append(itchat.search_friends(nickName = 'LHC'))
 
+for member in members:
+    if member['NickName'] in nickNames:
+        friends = itchat.search_friends(userName = member['UserName'])
+        for friend in friends:
+            print(friend)
+            ms.append(friend)
 
-ms.append(itchat.search_friends(nickName = '何继中 智能焊接机器人'))
-ms.append(itchat.search_friends(nickName = 'LHC'))
-
+print("*" * 80)
 print(ms)
-itchat.create_chatroom(ms, 'test backlist')
+chatroomUserName = itchat.create_chatroom(ms, 'test backlist')
+print(chatroomUserName)
+itchat.send('代码测试黑名单', chatroomUserName)
+os.sys.exit(1)
+
+f = open("rooms.txt", "w")
+rooms = itchat.get_chatrooms(update = True)
+for room in rooms:
+    print(room['NickName'])
+    f.write(str(room) + "\n")
+    if room['NickName'] == '▄█▀█●':
+        print("发送")
+        print(itchat.send('代码测试黑名单', room['UserName']))
+        os.sys.exit(1)
+f.close()
 
 #os.exit(1)
 
