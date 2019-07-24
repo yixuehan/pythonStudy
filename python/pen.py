297d62c9d5745bc65171919014ff09825c103623
desert <505703394@qq.com>
2019-07-24 20:45:46 +0800

    study

--- python/pen.py
+++ python/pen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import turtle
 
 turtle.forward(100)
---
+++ python/tturtle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import turtle
+
+
+t = turtle.Turtle()
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
