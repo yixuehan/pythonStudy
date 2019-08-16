ad614f4958a0f1edbf11869ebbe9595d7192eacc
desert <505703394@qq.com>
2019-08-16 18:00:53 +0800

    context

--- cpp/cmdline.cpp
+++ cpp/cmdline.cpp
@@ -9,20 +9,26 @@ using namespace boost;
 
 namespace po = program_options;
 
+#include "output.h"
+
 int main(int argc, char *argv[])
 {
     // po::options_description desc("Allowed options");
     po::options_description desc;
     desc.add_options()
         ("help", "this is help message")
-        ("file, F", po::value<string>(), "file full name")
+        ("file,F", po::value<string>(), "file full name")
         ("N", po::value<string>(), "name")
         (",H", po::value<int>(), "height")
         ("input-file", po::value<vector<string>>(), "input file")
         ;
 
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
+    // po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::command_line_parser(argc, argv).  options(desc).  positional(p).  run(), vm);
     po::notify(vm);
 
     if (vm.count("help")) {
@@ -32,7 +38,7 @@ int main(int argc, char *argv[])
 
     if (vm.count("input-file")) {
         cout << vm.count("input-file") << endl;
-        cout << (vm["input-file"].as<vector<string>>()) << endl;
+        cout << vm["input-file"].as<vector<string>>() << endl;
     }
 
     if (vm.count("file")) {
