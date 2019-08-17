a0cd64a764158c15d5461a394daa84e4e1866922
yixuehan <505703394@qq.com>
2019-08-17 19:46:26 +0800

    ctx

--- cpp/fiber/cal_fibon.cpp
+++ cpp/fiber/cal_fibon.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <functional>
 using namespace std;
 
 #include <boost/timer/timer.hpp>
-#include <boost/context/fiber.hpp>
+#include <boost/context/continuation.hpp>
 using namespace boost;
 
 namespace ctx = boost::context;
 
-ctx::fiber func(ctx::fiber &&sink, int &a)
+ctx::continuation func(ctx::continuation &&sink, int &a)
 {
     a = 0;
     int b = 1;
     for (;;) {
         cout << "func before resume:" << a << endl;
         getchar();
-        sink = std::move(sink).resume();
+        sink = sink.resume();
         cout << "func after resume:" << a << endl;
         int next = a + b;
         a = b;
@@ -26,10 +27,10 @@ ctx::fiber func(ctx::fiber &&sink, int &a)
 int main()
 {
     int a;
-    ctx::fiber source(std::bind(func, std::placeholders::_1, std::ref(a)));
+    ctx::continuation c = ctx::callcc(std::bind(func, std::placeholders::_1, std::ref(a)));
     for (int i = 0; i < 10; ++i) {
         cout << "main before resume:" << a  << endl;
-        source = std::move(source).resume();
+        c = c.resume();
         std::cout << "main after resume:" << a << endl ;
     }
 }
---
+++ cpp/fiber/ctx_fibon.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+#include <boost/timer/timer.hpp>
+#include <boost/context/fiber.hpp>
+using namespace boost;
+
+namespace ctx = boost::context;
+
+ctx::fiber func(ctx::fiber &&sink, int &a)
+{
+    a = 0;
+    int b = 1;
+    for (;;) {
+        // cout << "func before resume:" << a << endl;
+        getchar();
+        sink = std::move(sink).resume();
+        // cout << "func after resume:" << a << endl;
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    return std::move(sink);
+}
+
+int main()
+{
+    int a;
+    ctx::fiber source(std::bind(func, std::placeholders::_1, std::ref(a)));
+    for (int i = 0; i < 10; ++i) {
+        // cout << "main before resume:" << a  << endl;
+        source = std::move(source).resume();
+        // std::cout << "main after resume:" << a << endl ;
+    }
+}
