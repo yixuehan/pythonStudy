0bdbbad25126e2db62a79a8c20bac8377a6e6287
yixuehan <505703394@qq.com>
2019-10-11 19:50:20 +0800

    study

---
+++ cpp/memory/out_order.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+using namespace std;
+
+#include <boost/timer/timer.hpp>
+using namespace boost;
+
+atomic<int> x, y;
+
+void fn1()
+{
+    y = 1;
+    x = 2;
+    cout << x << ":" << y << endl;
+}
+
+void fn2()
+{
+    while (2 != x) {
+        int t = 1;
+        int w = t;
+        // cout << t << endl;
+        // cout << "x=" << x << endl;
+    }
+    cout << "while" << endl;
+    while(1 != y) {
+        // cout << "y=" << y << endl;
+    }
+    cout << "end" << endl;
+}
+
+int main()
+{
+    thread t2(fn2);
+    thread t1(fn1);
+    t1.join();
+    t2.join();
+}
