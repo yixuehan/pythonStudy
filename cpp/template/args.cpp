da064671843204040c8b99621d64e361f6db3c03
yixuehan <505703394@qq.com>
2019-11-28 18:49:54 +0800

    save

--- cpp/template/args.cpp
+++ cpp/template/args.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <boost/timer/timer.hpp>
 using namespace boost;
 
+/*
 template < typename T, typename ... Ts >
 void printf3(T &&value, Ts &&... args)
 {
@@ -12,9 +13,22 @@ void printf3(T &&value, Ts &&... args)
         printf3(std::forward<Ts>(args)...);
     }
 }
+*/
+
+template < typename ... Ts >
+auto printf3(Ts &&... args) ->void
+{
+       // std::cout << value << std::endl;
+    
+    std::initializer_list <int> {([](auto arg) {
+             std::cout << arg << std::endl;
+    }(args), 0) ...};
+
+}
+
 void test_lambda()
 {
- printf3(1, 2, 3);
+ printf3(1, 2, 3.12);
 }
 
 int main()
