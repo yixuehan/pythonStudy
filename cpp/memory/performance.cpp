21d33b93a8be088df0bcd7b06708ab3d1e78025b
yixuehan <505703394@qq.com>
2019-12-16 18:34:48 +0800

    save

--- cpp/memory/performance.cpp
+++ cpp/memory/performance.cpp
@@ -15,6 +15,17 @@ int lock_x = 0;
 constexpr int NUM = 10000;
 
 template <typename Data>
+void fn_add_load(Data &data)
+{
+    for (int i = 0; i < NUM; ++i) {
+        for (int j = 0; j < NUM; ++j) {
+            data.fetch_add(1, memory_order_relaxed);
+        }
+    }
+}
+
+
+template <typename Data>
 void fn_add(Data &data)
 {
     for (int i = 0; i < NUM; ++i) {
@@ -74,6 +85,11 @@ int main()
     {
         timer::auto_cpu_timer t;
         fn_thread(lock_x, fn_add_lock<int>);
+        printf("%d\n", lock_x);
+    }
+    {
+        timer::auto_cpu_timer t;
+        fn_thread(a_x, fn_add_load);
         printf("%d\n", lock_x);
     }
 
