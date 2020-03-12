24d79c7b55f5b09ad047249b7cf105b22e851ab4
yixuehan <505703394@qq.com>
2020-03-12 18:23:46 +0800

    save

--- cpp/network/asio_tcp_server.cpp
+++ cpp/network/asio_tcp_server.cpp
@@ -24,15 +24,14 @@ struct DataBuffer
     std::shared_ptr<char> buffer_ptr;
 };
 
-class TcpServer
+class TcpServerBase
 {
 public:
-    TcpServer(unsigned short port)
-        :_endpoint(ip::tcp::v4(), port),
+    TcpServerBase(unsigned short port)
+        :_io_context(1),
+         _endpoint(ip::tcp::v4(), port),
          _acceptor(_io_context, _endpoint)
     {
-        auto socket_ptr = std::make_shared<ip::tcp::socket>(_io_context);
-        _acceptor.async_accept(*socket_ptr, std::bind(&TcpServer::do_accept, this, socket_ptr, std::placeholders::_1));
     }
 
     void run()
@@ -40,11 +39,25 @@ public:
         _io_context.run();
     }
 
-private:
+protected:
+    asio::io_context _io_context;
+    ip::tcp::endpoint _endpoint;
+    ip::tcp::acceptor _acceptor;
+};
 
+class TcpServerJson : public TcpServerBase
+{
+public:
+    TcpServerJson(unsigned short port)
+        :TcpServerBase(port)
+    {
+        auto socket_ptr = std::make_shared<ip::tcp::socket>(_io_context);
+        _acceptor.async_accept(*socket_ptr, std::bind(&TcpServerJson::do_accept, this, socket_ptr, std::placeholders::_1));
+    }
     void do_accept(std::shared_ptr<ip::tcp::socket> socket_ptr, const system::error_code &ec)
     {
         if (ec) {
+            cout << ec.value() << endl;
             cout << ec.message() << endl;
             return;
         }
@@ -52,53 +65,58 @@ private:
 
         DataBuffer dataBuffer;
         async_read(*socket_ptr, buffer(reinterpret_cast<char*>(dataBuffer.length_ptr.get()), 4),
-                   std::bind(&TcpServer::do_read_length, this, socket_ptr, dataBuffer,
+                   std::bind(&TcpServerJson::do_read_length, this, socket_ptr, dataBuffer,
                    std::placeholders::_1, std::placeholders::_2));
 
         socket_ptr = std::make_shared<ip::tcp::socket>(_io_context);
-        _acceptor.async_accept(*socket_ptr, std::bind(&TcpServer::do_accept, this, socket_ptr, std::placeholders::_1));
+        _acceptor.async_accept(*socket_ptr, std::bind(&TcpServerJson::do_accept, this, socket_ptr, std::placeholders::_1));
     }
 
     void do_read_length(std::shared_ptr<ip::tcp::socket> socket_ptr, DataBuffer dataBuffer,
                         const boost::system::error_code &ec, size_t rbyte)
     {
         if (ec) {
+            cout << ec.value() << endl;
             cout << ec.message() << endl;
             return;
         }
-        // cout << "read length:" << rbyte << endl;
-        // cout << "body length:" << *dataBuffer.length_ptr << endl;
+        cout << "read length:" << rbyte << endl;
+        cout << "body length:" << *dataBuffer.length_ptr << endl;
         // dataBuffer.thread_id = std::this_thread::get_id();
         // assert (dataBuffer.thread_id == std::this_thread::get_id() && "接收与处理不在一个线程");
+#if 0
+        boost::system::error_code ec_body;
+        rbyte = read(*socket_ptr, buffer(dataBuffer.buffer_ptr.get(), *dataBuffer.length_ptr), ec_body);
+        do_read_body(socket_ptr, dataBuffer, ec_body, rbyte);
+#else
         async_read(*socket_ptr, buffer(dataBuffer.buffer_ptr.get(), *dataBuffer.length_ptr),
-                    std::bind(&TcpServer::do_read_body, this, socket_ptr, dataBuffer,
+                    std::bind(&TcpServerJson::do_read_body, this, socket_ptr, dataBuffer,
                     std::placeholders::_1, std::placeholders::_2));
+#endif
+        
     }
 
     void do_read_body(std::shared_ptr<ip::tcp::socket> socket_ptr, DataBuffer dataBuffer,
                       const boost::system::error_code &ec, size_t rbyte)
     {
         if (ec) {
+            cout << ec.value() << endl;
             cout << ec.message() << endl;
             return ;
         }
         // assert (dataBuffer.thread_id == std::this_thread::get_id() && "接收与处理不在一个线程");
         assert(static_cast<int>(rbyte) == *dataBuffer.length_ptr);
-        // cout << "rbyte:" << rbyte << endl;
-        // cout << "body:" << std::string(dataBuffer.buffer_ptr.get(), rbyte) << endl;
+        cout << "rbyte:" << rbyte << endl;
+        cout << "body:" << std::string(dataBuffer.buffer_ptr.get(), rbyte) << endl;
         async_read(*socket_ptr, buffer(reinterpret_cast<char*>(dataBuffer.length_ptr.get()), 4),
-                   std::bind(&TcpServer::do_read_length, this, socket_ptr, dataBuffer,
+                   std::bind(&TcpServerJson::do_read_length, this, socket_ptr, dataBuffer,
                    std::placeholders::_1, std::placeholders::_2));
     }
-private:
-    asio::io_context _io_context;
-    ip::tcp::endpoint _endpoint;
-    ip::tcp::acceptor _acceptor;
 };
 
 int main()
 {
-    TcpServer server(12345);
+    TcpServerJson server(12345);
     for (int i = 0; i < 100; ++i) {
         std::thread([&]{
                 server.run();
