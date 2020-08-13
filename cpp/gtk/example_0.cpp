93af633843e57093c903e0d36df2747b5895de6e
desert <wangk2@deepblueai.com>
2020-08-13 20:07:19 +0800

    save

--- cpp/gtk/example_0.cpp
+++ cpp/gtk/example_0.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     GtkApplication *app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(window);
+    g_object_disconnect(app, "activate", nullptr);
     g_object_unref(app);
     return status;
 }
